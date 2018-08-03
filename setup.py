6541b626a8e586a8aebc11a056994788162e266f
newpanjing <newpanjing@163.com>
2018-08-03 16:55:09 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='shortid',
+    version='1.0',
+    url='https://github.com/newpanjing/shortid',
+    license='Apache Licene 2.0',
+    author='shortid',
+    author_email='newpanjing@icloud.com',
+    description='根据uuid生成不重复的8位短id',
+    packages=['shortid'],
+    include_package_data=True,
+    platforms="any",
+)
